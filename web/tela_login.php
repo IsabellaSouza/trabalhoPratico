a26c8ec17aae4f3e673a7b96b57d49a1d0f842a6
lucasdds26 <lucas.dds@gmail.com>
2018-11-06 17:32:26 -0300

    Criada uma tela basica de login

---
+++ web/tela_login.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+  <title>Login</title>
+
+  <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+</head>
+<body>
+
+<?php
+        require_once('./nav.php');
+   ?>
+																												   
+ 
+  <div class="section no-pad-bot" id="index-banner">
+    <div class="container">
+      <br><br>
+													  <h1 class="header center orange-text">Login</h1>
+													  <div class="row center">
+														 <div class="row">
+															<form class="col s12">
+															  <div class="row">
+																										<div class="input-field col s6">
+																									  <input id="usuario" type="text" class="validate">
+																									  <label for="usuario">Usuário</label>
+																										</div>
+																										
+																								
+																									<div class="row">
+																										<div class="input-field col s6">
+																										<input id="password" type="password" class="validate">
+																											<label for="password">Senha</label>
+																											</div>
+																										</div>
+															 
+															  
+																										
+																								
+																									
+																										
+																										
+																										 
+															
+																									
+																											
+																											
+																										
+															  
+															  
+        
+																										  </div>
+																										  <div class="row center">
+																											<a href="http://materializecss.com/getting-started.html" id="download-button" class="btn-large waves-effect waves-light orange">Entrar</a>
+																											
+																										  </div>
+																										  <br><br>
+
+																										</div>
+																									  </div>
+																									  
+																									  
+																									  
+																									  
+
+  <div class="container">
+    <div class="section">
+
+      
+
+            
+          </div>
+        </div>
+      </div>
+
+    </div>
+    <br><br>
+  </div>
+																								<!--rodape-->	
+																				  <footer class="page-footer orange">
+																					<div class="container">
+																					  <div class="row">
+																						<div class="col l6 s12">
+																						  <h5 class="white-text"></h5>
+																						  <p class="grey-text text-lighten-4"></p>
+
+
+																									</div>
+																									<div class="col l3 s12">
+																									  <h5 class="white-text"></h5>
+																									  <ul>
+																										<li><a class="white-text" href="#!"></a></li>
+																										<li><a class="white-text" href="#!"></a></li>
+																										<li><a class="white-text" href="#!"></a></li>
+																										<li><a class="white-text" href="#!"></a></li>
+																									  </ul>
+																									</div>
+																									<div class="col l3 s12">
+																									  <h5 class="white-text"></h5>
+																									  <ul>
+																										<li><a class="white-text" href="#!"></a></li>
+																										<li><a class="white-text" href="#!"></a></li>
+																										<li><a class="white-text" href="#!"></a></li>
+																										<li><a class="white-text" href="#!"></a></li>
+																									  </ul>
+																									</div>
+																								  </div>
+																								</div>
+																								<div class="footer-copyright">
+																								  <div class="container">
+																								   <a class="orange-text text-lighten-3" href="http://materializecss.com"></a>
+																								  </div>
+																								</div>
+																							  </footer>
+
+
+  <!--  Scripts-->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="js/materialize.js"></script>
+  <script src="js/init.js"></script>
+
+  </body>
+</html>
