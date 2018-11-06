f6ad7558b6eafbd2a917a584d29f6307df594a62
RodrigoCl <RodrigoCl@LAB49-05.lab.unigran>
2018-11-05 22:41:18 -0300

    Página inicial criada, falta apenas terminar o formulário e fazer as validações necessárias

---
+++ web/index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="pt">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+        <title>Locação</title>
+
+        <!-- CSS  -->
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+        <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+    </head>
+    <body>
+        <nav class="light-blue lighten-1" role="navigation">
+            <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">Locação de Veículos</a>
+                <ul class="right hide-on-med-and-down">
+                    <li><a href="#">Locações</a></li>
+                    <li><a href="#">Veículos</a></li>
+                    <li><a href="#">Clientes</a></li>
+                    <li><a href="#">Funcionários</a></li>
+                </ul>
+
+                <ul id="nav-mobile" class="sidenav">
+                    <li><a href="#">Locações</a></li>
+                    <li><a href="#">Veículos</a></li>
+                    <li><a href="#">Clientes</a></li>
+                    <li><a href="#">Funcionários</a></li>
+                </ul>
+                <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+            </div>
+        </nav>
+        <div class="section no-pad-bot" id="index-banner">
+            <div class="container">
+                <br><br>
+                <h1 class="header left orange-text">Aluguel de Carros</h1>
+                <div class="row center">
+                    <h5 class="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
+                </div>
+                <div class="row center">
+                    <a href="http://materializecss.com/getting-started.html" id="download-button" class="btn-large waves-effect waves-light orange">Get Started</a>
+                </div>
+                <br><br>
+
+                <!--FORMULARIO DE BUSCA DE VEICULOS-->
+                <div class="row">
+                    <form class="col s12">
+                        <div class="row">
+                            
+                            <div class="input-field col s6">
+                                <i class="material-icons prefix">location_on</i>
+                                <input id="icon_prefix" type="text" class="validate">
+                                <label for="icon_prefix">Local</label>
+                            </div>
+                            <div class="input-field col s6">
+                                <input type="text" class="datepicker">
+                                <label for="icon_prefix">Data</label>
+                            </div>
+                            
+                        </div>
+                        
+                        
+                        
+                        
+                    </form>
+                </div>
+
+            </div>
+        </div>
+
+
+
+
+        <footer class="page-footer orange">
+            <div class="container">
+                <div class="row">
+                    <div class="col l6 s12">
+                        <h5 class="white-text">Company Bio</h5>
+                        <p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
+
+
+                    </div>
+                    <div class="col l3 s12">
+                        <h5 class="white-text">Settings</h5>
+                        <ul>
+                            <li><a class="white-text" href="#!">Link 1</a></li>
+                            <li><a class="white-text" href="#!">Link 2</a></li>
+                            <li><a class="white-text" href="#!">Link 3</a></li>
+                            <li><a class="white-text" href="#!">Link 4</a></li>
+                        </ul>
+                    </div>
+                    <div class="col l3 s12">
+                        <h5 class="white-text">Connect</h5>
+                        <ul>
+                            <li><a class="white-text" href="#!">Link 1</a></li>
+                            <li><a class="white-text" href="#!">Link 2</a></li>
+                            <li><a class="white-text" href="#!">Link 3</a></li>
+                            <li><a class="white-text" href="#!">Link 4</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-copyright">
+                <div class="container">
+                    Made by <a class="orange-text text-lighten-3" href="http://materializecss.com">Materialize</a>
+                </div>
+            </div>
+        </footer>
+
+
+        <!--  Scripts-->
+        <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+        <script src="/web/js/materialize.js"></script>
+        <script src="/web/js/init.js"></script>
+
+    </body>
+</html>
