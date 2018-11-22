41041bda54106cc71305d3b69f853f6fd6eabb15
IsabellaSouza <802.006@alunos.unigran.br>
2018-11-22 20:04:48 -0300

    Recriação das Telas: IsabellaA@LAB49-14 MINGW64 //gandalf/www/IsabellaA/trabalhoPratico (master)

---
+++ mobile/app/src/main/java/com/example/isabellaa/localplus/cadastro_funcionario.java
@@ -0,0 +1,13 @@
+package com.example.isabellaa.localplus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class cadastro_funcionario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_funcionario);
+    }
+}
---
+++ mobile/app/src/main/java/com/example/isabellaa/localplus/tabela_locacao.java
@@ -0,0 +1,13 @@
+package com.example.isabellaa.localplus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class tabela_locacao extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tabela_locacao);
+    }
+}
