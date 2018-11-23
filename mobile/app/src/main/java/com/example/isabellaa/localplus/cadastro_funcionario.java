9fca544dae8d9d6277036cef23176c2438352912
lucasdds26 <lucas.dds@gmail.com>
2018-11-23 16:40:47 -0300

    Feito o layout do cadastro de funcionarios e tabela

--- mobile/app/src/main/java/com/example/isabellaa/localplus/cadastro_funcionario.java
+++ mobile/app/src/main/java/com/example/isabellaa/localplus/cadastro_funcionario.java
@@ -2,6 +2,7 @@ package com.example.isabellaa.localplus;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class cadastro_funcionario extends AppCompatActivity {
 
@@ -9,5 +10,15 @@ public class cadastro_funcionario extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_funcionario);
+    }
+
+    public void NovoFuncionario(View view){
+
+
+    }
+
+    public void SairFuncionario(View view){
+
+
     }
 }
