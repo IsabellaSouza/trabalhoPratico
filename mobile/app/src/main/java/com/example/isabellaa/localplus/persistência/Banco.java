d6b97bfd6499ef4c6e7d5f6e9c44ad2c24067c81
FRodriguesB <frfelipe45@gmail.com>
2018-11-26 02:55:33 -0200

    Desenho das Telas de Cadastrar e Listar Clientes, Implementação das ações de salvar e excluir cliente

--- mobile/app/src/main/java/com/example/isabellaa/localplus/persistência/Banco.java
+++ mobile/app/src/main/java/com/example/isabellaa/localplus/persistência/Banco.java
@@ -7,19 +7,23 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class Banco extends SQLiteOpenHelper {
     public Banco(Context context){super (context, "BANCODADOS", null, 3);}
 
+
+
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
   String sql = "CREATE TABLE IF NOT EXISTS CARRO(" +"ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +"PLACA VARCHAR(7)," +"NOME VARCHAR(50),"+ "MARCA VARCHAR(50),"+"MODELO VARCHAR(50),"+ "VALORDOSEGURO FLOAT,"+"VALORDALOCACAO FLOAT, "+"COR VARCHAR(20), "+"ATIVO BOOLEAN)";
-   sqLiteDatabase.execSQL(sql);
-
+  String sqlcliente = "CREATE TABLE IF NOT EXISTS CLIENTE(" +"ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +"NOME VARCHAR(7)," +"RG VARCHAR(50),"+ "CPF VARCHAR(50),"+"ENDERECO VARCHAR(50),"+ "CNH INTERGER,"+"NUMERODEPENDENTES INTEGER)";
+  sqLiteDatabase.execSQL(sql);
+  sqLiteDatabase.execSQL(sqlcliente);
         sqLiteDatabase.execSQL(sql);
+        sqLiteDatabase.execSQL(sqlcliente);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
         String sql="ALTER TABLE CARRO ADD PLACA VARCHAR(7);";
-
+        String sqlcliente="ALTER TABLE CLIENTE ADD NOME VARCHAR(7)";
         sqLiteDatabase.execSQL(sql);
-
+        sqLiteDatabase.execSQL(sqlcliente);
     }
 }
--- mobile/app/src/main/java/com/example/isabellaa/localplus/persistência/Dao.java
+++ mobile/app/src/main/java/com/example/isabellaa/localplus/persistência/Dao.java
@@ -1,14 +1,16 @@
 package com.example.isabellaa.localplus.persistência;
 
 import com.example.isabellaa.localplus.entidade.Carro;
+import com.example.isabellaa.localplus.entidade.Cliente;
 
 import java.util.LinkedList;
 import java.util.List;
 
 public class Dao {
     private static List lista;
+    private static List listacliente;
     private static int indice;
-
+    private static int indicecliente;
 
     private Dao(){}
 
@@ -22,6 +24,16 @@ public class Dao {
         }
     }
 
+    public static void salvar(Cliente c){
+        if(listacliente.contains(c))
+            listacliente.set(listacliente.indexOf(c),c);
+        else{
+            c.setId(indicecliente);
+            listacliente.add(c);
+            indicecliente++;
+        }
+    }
+
     public static List getLista(){
 
         if(lista==null){
@@ -31,5 +43,12 @@ public class Dao {
         return lista;
 
     }
+    public static List getListacliente(){
 
+        if(listacliente==null){
+            listacliente= new LinkedList();
+            indicecliente = 0;
+        }
+        return listacliente;
+    }
 }
